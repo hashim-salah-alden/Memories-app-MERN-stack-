ac46881520db584487d9dfe2fc78dd05231c14e5
hashim-salah-alden <82958890+hashim-salah-alden@users.noreply.github.com>
2024-10-13 02:10:09 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import path from "path";
+const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");

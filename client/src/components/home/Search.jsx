c9212e73055f7a5efaf9e7ab91620e2291991151
hashim-salah-alden <82958890+hashim-salah-alden@users.noreply.github.com>
2024-10-13 20:18:41 +0300

    Update Search.jsx

--- client/src/components/home/Search.jsx
+++ client/src/components/home/Search.jsx
@@ -49,31 +49,32 @@ const Search = () => {
     setSearched(false);
   };
 
-
   return (
     <div className="flex justify-between items-center gap-2">
-      <form onSubmit={handleSearch}>
-        <label className="input input-bordered flex items-center gap-2 bg-[#1f3395]">
-          <input
-            type="text"
-            placeholder="Search"
-            onChange={(e) => handleChange(e)}
-            className=" placeholder-slate-300 text-slate-50"
-            value={search}
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70 text-slate-50"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
+      <div className="flex bg-[#1f3395] rounded-xl max-w-[70%] pr-2">
+        <form className="grow max-w-[85%]" onSubmit={handleSearch}>
+          <label className="input input-bordered flex items-center gap-2 bg-[#1f3395]">
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={(e) => handleChange(e)}
+              className=" placeholder-slate-300 text-slate-50"
+              value={search}
             />
-          </svg>
-        </label>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70 text-slate-50"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+        </form>
         {searched && (
           <button
             className="btn btn-circle bg-opacity-0 border-0 text-white"
@@ -82,9 +83,9 @@ const Search = () => {
             <MdOutlineCancel onC className="w-6 h-6 outline-none" />
           </button>
         )}
-      </form>
+      </div>
 
-      <label className="form-control w-24 max-w-xs ">
+      <label className="form-control   max-w-[25%] ">
         <select
           onChange={handleFilter}
           className="select select-bordered bg-[#1f3395] text-slate-50"
